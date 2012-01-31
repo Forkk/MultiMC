237933503dc3ad9c85d46fd08e4d23ac01cc3259
ShaRose <ShawnAndrewRose@Gmail.Com>
2012-01-30 22:55:40 -0330

    Updated the formatting.

--- MultiMC/Java Launcher/src/MultiMCLauncher.java
+++ MultiMC/Java Launcher/src/MultiMCLauncher.java
@@ -5,41 +5,49 @@ import java.lang.reflect.Modifier;
 
 public class MultiMCLauncher {
 	/**
-	 * @param args The arguments you want to launch Minecraft with. New path, Username, Session ID.
+	 * @param args
+	 *            The arguments you want to launch Minecraft with. New path,
+	 *            Username, Session ID.
 	 */
 	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
-		if(args.length != 3)
+		if (args.length != 3) {
 			System.exit(-1);
+		}
 		try {
 			@SuppressWarnings("rawtypes")
-			Class mc = Class.forName("net.minecraft.client.Minecraft"); // Get the Minecraft Class.
-			if(mc == null)
-				throw new ClassNotFoundException(); // This shouldn't ever be called, but just in case.
-				Field[] fields = mc.getDeclaredFields();
+			// Get the Minecraft Class.
+			Class mc = Class.forName("net.minecraft.client.Minecraft");
 
-				for (int i = 0; i < fields.length; i++) {
-					Field f = fields[i];
-					if (f.getType() != File.class) { // Has to be File.
-						continue;
-					}
-					if (f.getModifiers() != Modifier.PRIVATE + Modifier.STATIC) { // And Private Static.
-						continue;
-					}
-					f.setAccessible(true);
-					f.set(null, new File(args[0])); // And set it.
-					System.out.println("Fixed Minecraft Path: Field was "
-							+ f.toString());
-				}
-				
-				String[] mcArgs = new String[2];
-				mcArgs[0] = args[1];
-				mcArgs[1] = args[2];
-				
-				mc.getMethod("main", String[].class).invoke(null, (Object)mcArgs);
+			if (mc == null) {
+				// This shouldn't ever be called, but just in case.
+				throw new ClassNotFoundException();
 			}
-		catch(ClassNotFoundException e)
-		{
+			Field[] fields = mc.getDeclaredFields();
+
+			for (int i = 0; i < fields.length; i++) {
+				Field f = fields[i];
+				if (f.getType() != File.class) {
+					// Has to be File
+					continue;
+				}
+				if (f.getModifiers() != (Modifier.PRIVATE + Modifier.STATIC)) {
+					// And Private Static.
+					continue;
+				}
+				f.setAccessible(true);
+				f.set(null, new File(args[0]));
+				// And set it.
+				System.out.println("Fixed Minecraft Path: Field was "
+						+ f.toString());
+			}
+
+			String[] mcArgs = new String[2];
+			mcArgs[0] = args[1];
+			mcArgs[1] = args[2];
+
+			mc.getMethod("main", String[].class).invoke(null, (Object) mcArgs);
+		} catch (ClassNotFoundException e) {
 			System.exit(1);
 		} catch (IllegalArgumentException e) {
 			System.exit(2);
