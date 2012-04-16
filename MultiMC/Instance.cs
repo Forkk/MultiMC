54b94de9a23d338b82319562c448f3010a6c574a
Petr Mrázek <peterix@gmail.com>
2012-04-16 13:16:36 +0200

    Fix one more problem with mod indexes being -1

--- MultiMC/Instance.cs
+++ MultiMC/Instance.cs
@@ -815,10 +815,10 @@ namespace MultiMC
 						File.Copy(path, dest, true); // true = overwrite
 						if (!modList.Any(m => m.FileName == path))
 						{
-							//if (index <= 0)
-							//    Add(new Mod(dest), true);
-							//else
-							Insert(index, new Mod(dest), true);
+							if (index == -1)
+							    Add(new Mod(dest), true);
+							else
+								Insert(index, new Mod(dest), true);
 						}
 					}
 					finally
@@ -851,8 +851,7 @@ namespace MultiMC
 		{
 			if (modList.Any(m => m.FileName == mod.FileName))
 			{
-				//Console.WriteLine("{0} was added to modlist twice. Removed original.",
-				//    mod.FileName);
+				Console.WriteLine("{0} was added to modlist twice. Removed original.", mod.FileName);
 				Remove(modList.Where(m => m.FileName == mod.FileName), false);
 			}
 
